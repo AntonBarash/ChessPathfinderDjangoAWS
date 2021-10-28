e5a706c57ea28c25b6a3caf6784f33368a29312c
AntonBarash <64368628+AntonBarash@users.noreply.github.com>
2021-10-28 13:30:08 -0400

    Update settings.py

--- django3/settings.py
+++ django3/settings.py
@@ -19,7 +19,7 @@ ENV_PATH = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-muy1sez6&hmsv^!9x)_uq!+&u2-r#=o5!)$-ucs#xqf=$(yx)4'
+SECRET_KEY = ''#secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
